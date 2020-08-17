c5121cc639c2a615dcbde16a238613b70ae93ab8
n-ramadan <61492011+n-ramadan@users.noreply.github.com>
2020-08-18 01:09:43 +0700

    Update to match example from Dinesh

--- question1.py
+++ question1.py
@@ -1,21 +1,16 @@
 # Split a file which has n number of schema def. And store them in a dict of lists(no 3rd party imports)
-# File to read: to_read.txt
+# File to read: to_read.txt / question1_text.txt
 
 
 #Set first field value as dictionary key
 dic1 = {}
-with open("to_read.txt", "r") as file:
+with open("question1_text.txt", "r") as file:
     for line in file:
         line_split = line.split(',')
-        line_split = [x.strip(" ").strip("\n") for x in line_split]
-        dic1[line_split[0]] = line_split[1:]
-
-#Set ordered integer as dictionary key
-dic2 = {}
-key = 0
-with open("to_read.txt", "r") as file:
-    for line in file:
-        line_split = line.split(',')
-        line_split = [x.strip(" ").strip("\n") for x in line_split]
-        dic2[key] = line_split
-        key = key + 1
+        line_split = [x.strip(" ").strip("\n") for x in line_split] #clean trailing whitespaces
+        key = line_split[0]
+        try:
+            dic1[key].append(line_split[1:])
+        except:
+            dic1[key] = []
+            dic1[key].append((line_split[1:]))
